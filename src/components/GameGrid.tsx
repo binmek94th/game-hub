4906784899bed09c428a8115755c60d38825f62d
Nehemiah Mekonnen <binyamm83@gmail.com>
2024-07-30 12:41:05 +0300

    clean up the game card

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -25,8 +25,8 @@ const GameGrid = ({ gameQuery}: Props) => {
                 sm: 1,
                 md: 2,
                 lg: 3,
-                xl: 5
-            }} padding='10px' spacing={3}>
+                xl: 4
+            }} padding='10px' spacing={6}>
                 {isLoading &&
                     skeletons.map((skeleton) =>(
                         <GameCardContainer key={skeleton}>
